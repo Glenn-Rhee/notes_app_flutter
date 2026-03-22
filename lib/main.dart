e1e4ca605f27710e93cfdeac8a648fd765da8428
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-22 21:19:04 +0700

    feat: added animation slider for add note page

--- lib/main.dart
+++ lib/main.dart
@@ -91,6 +91,22 @@ class MyApp extends StatelessWidget {
               PageRouteBuilder(
                 pageBuilder: (context, animation, secondaryAnimation) =>
                     const AddNote(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                      const begin = Offset(1.0, 0.0);
+                      const end = Offset.zero;
+                      const curve = Curves.ease;
+
+                      var tween = Tween(
+                        begin: begin,
+                        end: end,
+                      ).chain(CurveTween(curve: curve));
+
+                      return SlideTransition(
+                        position: animation.drive(tween),
+                        child: child,
+                      );
+                    },
               ),
             ),
           },
