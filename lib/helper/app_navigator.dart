b16737fa15be28c43b8e87829a72d09f45876c46
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-23 21:22:35 +0700

    refactor: added logic at goToAddNote

--- lib/helper/app_navigator.dart
+++ lib/helper/app_navigator.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/page/add_note.dart';
 
 class AppNavigator {
-  static void goToAddNote(BuildContext context){
-    
+  static void goToAddNote(BuildContext context) {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            const AddNote(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
+
+          var tween = Tween(
+            begin: begin,
+            end: end,
+          ).chain(CurveTween(curve: curve));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
   }
 }
