c59fd15b44946153d0bb946cd1f709cc6e86b9b2
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-23 21:22:01 +0700

    refactor: delete handler at on pressed

--- lib/helper/app_navigator.dart
+++ lib/helper/app_navigator.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppNavigator {
   static void goToAddNote(BuildContext context){
-
+    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -87,28 +87,7 @@ class MyApp extends StatelessWidget {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {
-            Navigator.of(context).push(
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    const AddNote(),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                      const begin = Offset(1.0, 0.0);
-                      const end = Offset.zero;
-                      const curve = Curves.ease;
-
-                      var tween = Tween(
-                        begin: begin,
-                        end: end,
-                      ).chain(CurveTween(curve: curve));
-
-                      return SlideTransition(
-                        position: animation.drive(tween),
-                        child: child,
-                      );
-                    },
-              ),
-            ),
+           
           },
           child: Icon(Icons.add),
         ),
