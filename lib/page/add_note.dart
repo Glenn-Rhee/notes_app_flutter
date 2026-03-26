7f8407c579c277cd6ac99a94370e1b53af019559
Glenn-Rhee <glennviktor5@gmail.com>
2026-03-26 22:03:59 +0700

    refactor: added contentController

--- lib/page/add_note.dart
+++ lib/page/add_note.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AddNote extends StatelessWidget {
   final TextEditingController titleController = TextEditingController();
+  final TextEditingController contentController = TextEditingController();
   const AddNote({super.key});
 
   @override
