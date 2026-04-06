224c815a7ba4965c0644c35cc939f3c67508c420
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-06 14:39:27 +0700

    refactor: update model Note

--- lib/notes.dart
+++ lib/notes.dart
@@ -14,15 +14,23 @@ class Note {
     required this.id,
     required this.createdAt,
   });
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
 }
 
 class Notes {
   Future<List<Note>?> getNotes() async {
     final url = Uri.parse("http://10.0.2.2:8080/notes");
     final response = await http.get(url);
-    if (response.statusCode == 200 || response.statusCode == 201){
+    if (response.statusCode == 200 || response.statusCode == 201) {
       final dataLists = jsonDecode(response.body)["data"];
-      
     }
     return [
       Note(
