1408df38390604fb863f99f7d051b594c406d521
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-07 20:57:50 +0700

    refactor: update notes.dart

--- lib/notes.dart
+++ lib/notes.dart
@@ -31,7 +31,7 @@ class Notes {
     final response = await http.get(url);
     if (response.statusCode == 200 || response.statusCode == 201) {
       final dataLists = jsonDecode(response.body)["data"] as List;
-      return dataLists.map((data) => Note.fromJson(data))
+      return dataLists.map((data) => Note.fromJson(data)).toList();
     }
     return [
       Note(
